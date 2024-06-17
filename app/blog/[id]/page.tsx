7a26fd6be66e50e91ef47929713d390b7c62306b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 19:25:21 +0530

    Initialization (const { Products } = defineOneEntry(process.env.API_URL!, {token:process.env.API_TOKEN, langCode:"en_US"});):
    Products is destructured from the result of defineOneEntry. It represents an object or service obtained from the defined entry point (defineOneEntry). This object likely provides methods or functions to interact with product data or articles on an external API.
    process.env.API_URL!, process.env.API_TOKEN, and 'en_US' are passed as parameters to defineOneEntry. These likely represent environment variables used for API authentication and localization.

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -3,7 +3,7 @@ import { defineOneEntry } from 'oneentry';
 import React from 'react'
 
 
-
+const { Products } = defineOneEntry(process.env.API_URL!, {token:process.env.API_TOKEN, langCode:"en_US"});
 
 
 
