aa68227ed67d06dae63489576411bdddea801d26
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 19:25:49 +0530

    Async Data Fetching (const blogId = params.id;, const value = await Products.getProductById(blogId, 'en_US');):
    blogId extracts the id parameter from params, which presumably identifies the specific blog or article to fetch.
    Products.getProductById(blogId, 'en_US') makes an asynchronous call to getProductById method provided by Products (obtained from defineOneEntry).
    'en_US' specifies the language code (English, United States) for localization purposes.
    value stores the result of this asynchronous operation, which likely contains data related to the specified blog or article.

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -6,8 +6,8 @@ import React from 'react'
 const { Products } = defineOneEntry(process.env.API_URL!, {token:process.env.API_TOKEN, langCode:"en_US"});
 
 const SingleArticle = async ({params}:any) => {
-    
-    
+    const blogId = params.id;
+    const value = await Products.getProductById(blogId, 'en_US');
  
     
 
