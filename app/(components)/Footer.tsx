6aa6a2a658da9180557d000d529da5ece9ce2d0a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 19:11:24 +0530

    Return Statement (return (...)):
    The Footer component returns JSX, describing the UI of the footer section of the application.
    JSX Elements (<footer>, <div>, <h3>, <ul>, <li>, <Link>, <a>):
    
    The JSX returned by Footer:
    Starts with a <footer> element with a class name "text-black py-2", providing styling for text color and padding.
    Inside the <footer>, there's a <div> with class name "container mx-auto flex flex-col lg:flex-row justify-between items-center", organizing content into a responsive grid layout.
    Within this <div>, there are three child <div> elements, each representing a section (Explore, Connect, Legal) of the footer.
    Each section contains an <h3> element for the section title ("Explore", "Connect", "Legal"), styled with "text-lg font-semibold".
    Below each <h3>, there's a <ul> element containing a list of <li> elements.
    For the Explore section, <li> elements contain <Link> components that dynamically generate navigation links based on the menus array fetched from the API. Each <Link> component has an href attribute pointing to the respective page URL (/${menu.pageUrl === "home" ? "/" : menu.pageUrl}).
    For the Connect and Legal sections, <li> elements contain <a> elements with href="#", indicating placeholder links. These could be external links to social media platforms or legal documents.

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -12,5 +12,35 @@ const Footer = async () => {
  
     
 
+  return (
+    <footer className="text-black py-2">
+      <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
+        <div className="text-center lg:text-left mb-4 lg:mb-0">
+          <h3 className="text-lg font-semibold">Explore</h3>
+          <ul className="mt-4">
+            {
+              menus && menus.map((menu:any) => <li key={menu.id}><Link href={`/${menu.pageUrl === "home" ? "/" : menu.pageUrl}`}>{menu.pageUrl.toUpperCase()}</Link></li>)
+            }
+          </ul>
+        </div>
+        <div className="text-center mb-4 lg:mb-0">
+          <h3 className="text-lg font-semibold">Connect</h3>
+          <ul className="mt-4">
+            <li><a href="#" >TWITTER</a></li>
+            <li><a href="#" >FACEBOOK</a></li>
+            <li><a href="#" >INSTAGRAM</a></li>
+          </ul>
+        </div>
+        <div className="text-center">
+          <h3 className="text-lg font-semibold">LEGAL</h3>
+          <ul className="mt-4">
+            <li><a href="#" >PRIVACY POLICY</a></li>
+            <li><a href="#" >TERMS OF USE</a></li>
+          </ul>
+        </div>
+      </div>
+     
+    </footer>
+  )
 }
 
