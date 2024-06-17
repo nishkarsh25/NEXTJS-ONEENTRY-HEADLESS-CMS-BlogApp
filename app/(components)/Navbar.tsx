4a1e973d557651ed5f8f788be882f3cfe9f1167f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 19:08:49 +0530

    Return Statement (return (...)):
    The Navbar component returns JSX, describing the UI of the navbar.
    JSX Elements (<div>, <h1>, <Link>, <ContactButton>):
    
    The JSX returned by Navbar:
    Starts with a <div> element that acts as the container for the navbar. It has CSS classes ('flex items-center justify-between max-w-6xl mx-auto h-20') to control layout and styling.
    Inside this <div>, there are two child <div> elements:
    The first child <div> ('md:mx-0 mx-2') contains an <h1> element with text "Blog App". This is likely the branding or title of the application.
    The second child <div> ('md:block hidden') is conditionally shown only on medium screens (md:block). It contains a <div> with a set of <Link> components.
    These <Link> components are generated dynamically based on the pages array fetched from the API.
    Each <Link> represents a navigation item (page.localizeInfos.menuTitle) and has a href attribute that points to the corresponding page URL (/${page.pageUrl === "home" ? "/" : page.pageUrl}).
    The key attribute is set to page.id to uniquely identify each <Link> component in React's rendering process.
    Finally, outside of this conditional <div>, there's a <ContactButton> component rendered. This likely provides a button or link for contacting or accessing contact-related functionality.

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -10,7 +10,22 @@ const Navbar = async() => {
     const pages = await Pages.getPages('en_US')
     
 
-  
+  return (
+    <div className='flex items-center justify-between max-w-6xl mx-auto h-20'>
+             <div className='md:mx-0 mx-2'>
+                <h1 className='text-2xl font-bold'>Blog App</h1>
+             </div>
+              {/* menu items */}
+             <div className='md:block hidden'>
+                    <div className='flex items-center gap-8'>
+                        {
+                            pages && pages.map((page:any)=> <Link key={page.id} href={`/${page.pageUrl === "home" ? "/" : page.pageUrl}`}>{page?.localizeInfos?.menuTitle}</Link>)
+                        }
+                    </div>
+             </div>
+             <ContactButton/>
+        </div>
+  )
 }
 
 
