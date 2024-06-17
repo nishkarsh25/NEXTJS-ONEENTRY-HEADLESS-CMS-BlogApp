ac6aafe72d46fd4b90a632bc2f6b4d5b96581408
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 19:26:10 +0530

    Return Statement (return (...)):
    The SingleArticle component returns JSX, describing the UI structure of the single article view.
    JSX Elements (<div>, <Image>, <h1>, <p>):
    
    The JSX returned by SingleArticle:
    Starts with a <div> element with class name 'w-full border-b mb-5', defining a container for the article content with a bottom border and margin.
    Inside the <div>, another <div> with class 'md:w-[50%] w-full m-auto' is used to center align the content horizontally.
    <Image> component is used to display the article thumbnail:
    src={thumbnail}: Sets the source URL for the image.
    alt="blog-image": Specifies the alternative text for accessibility purposes.
    width={500} and height={500}: Sets the dimensions of the image.
    className='w-full object-contain my-5': Applies styling to ensure the image fills its container and maintains aspect ratio.
    <h1> with class 'text-3xl font-medium text-center' displays the article title (title) in a large, centered format.
    <div> with class 'my-5' contains a <p> element:
    className='m-0': Sets margin to zero.
    dangerouslySetInnerHTML={{__html: blogContent}}: Renders the HTML content of blogContent dangerously, allowing formatted content to be displayed as-is.

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -13,6 +13,23 @@ const SingleArticle = async ({params}:any) => {
     const thumbnail = value?.attributeValues?.blogthumbnail?.value?.downloadLink;
     const blogContent =  value?.attributeValues?.blogcontent?.value?.htmlValue;
 
-    
+    return (
+        <div className='w-full border-b mb-5'>
+            <div className='md:w-[50%] w-full m-auto'>
+                <Image
+                    src={thumbnail}
+                    alt="blog-image"
+                    width={500}
+                    height={500}
+                    className='w-full object-contain my-5'
+                />
+                <h1 className='text-3xl font-medium text-center'>{title}</h1>
+                <br />
+                <div className='my-5'>
+                        <p className='m-0' dangerouslySetInnerHTML={{__html:blogContent}}/>
+                </div>
+            </div>
+        </div>
+    )
 }
 
