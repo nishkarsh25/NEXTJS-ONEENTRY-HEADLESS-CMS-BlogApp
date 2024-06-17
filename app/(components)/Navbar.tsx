0d9fa80a0a167bdd3fb0ca3bb084a6bab200a81f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 19:08:07 +0530

    OneEntry API Initialization (const { Pages } = defineOneEntry(process.env.API_URL!, {token:process.env.API_TOKEN, langCode:"en_US"});):
    This line initializes the OneEntry API using the defineOneEntry function.
    process.env.API_URL and process.env.API_TOKEN are environment variables that likely contain the URL and token required to access the OneEntry API.
    { langCode: "en_US" } specifies the language code for which pages will be retrieved (in this case, English - United States).
    Pages is then extracted from the result of defineOneEntry. This object or function likely provides methods to fetch pages or content from the OneEntry API.

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ContactButton from './ContactButton'
 import { defineOneEntry } from 'oneentry';
 
-
+const { Pages } = defineOneEntry(process.env.API_URL!, {token:process.env.API_TOKEN, langCode:"en_US"});
 
 
 
