d74053c67b565956cfec4ba559adacde75a77df7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 17:32:34 +0530

    JSX Element (<div> ... </div>):
    The return statement encloses JSX within parentheses (...).
    The JSX consists of a <div> element, which is a container element in HTML/React used to group elements together.
    Heading Element (<h1 className='text-4xl font-bold text-center py-20'>...</h1>):
    
    Inside the <div>, there's an <h1> element.
    Attributes like className are used instead of class in JSX to define CSS classes. In this case, text-4xl, font-bold, text-center, and py-20 are Tailwind CSS classes.
    text-4xl sets the font size to extra-large, font-bold makes the text bold, text-center aligns the text in the center horizontally, and py-20 adds padding (py-20 is shorthand for py-5 which adds padding-top and padding-bottom equivalent to 5rem).
    Text Content (Page Not Found 404):
    
    Inside the <h1> element, the text "Page Not Found 404" is displayed.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const NotFoundPage = () => {
   return (
-    <></>
+    <div>
+        <h1 className='text-4xl font-bold text-center py-20'>Page Not Found 404</h1>
+    </div>
   )
 }
 
