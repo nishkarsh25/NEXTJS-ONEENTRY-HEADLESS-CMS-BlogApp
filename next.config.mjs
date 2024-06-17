03049b1bbaab09c5b5cc662157862a626d03cb9f
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 17:07:07 +0530

    Configured nextjs with typescript and shadcn:
    Also added my .env file which will contain my oneEntry Headless cms api toekn's:
    
    Added a demo image in public folder:

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                hostname:'applicationblog.oneentry.cloud'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
