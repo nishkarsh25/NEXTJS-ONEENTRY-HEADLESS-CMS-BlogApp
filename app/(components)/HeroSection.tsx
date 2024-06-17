d5a1ab3bb3a3d1acd46731f53ff6a2bbe3f40ca7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 19:12:42 +0530

    Data Processing (const data = pages && pages.find((page:any)=> page.pageUrl === "home")?.attributeValues;):
    Inside the component, it first checks if pages exists (pages && ...). This guards against errors in case pages is null or undefined.
    It uses the find method to search for a page where pageUrl is "home". This assumes there's a page in the pages array with the URL "home".
    If such a page is found, it accesses its attributeValues property and assigns it to data. This data variable now likely contains specific attributes related to the "home" page, fetched from the API.

--- app/(components)/HeroSection.tsx
+++ app/(components)/HeroSection.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 
 const HeroSection = ({pages}:{pages:any}) => {
-  
+  const data = pages && pages.find((page:any)=> page.pageUrl === "home")?.attributeValues;
   
   
 }
