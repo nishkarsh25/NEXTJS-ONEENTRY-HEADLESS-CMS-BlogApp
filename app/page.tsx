7a5440f51bfaae75f2a7fba92c24594b85988f85
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 19:05:50 +0530

    API Call (const pages = await Pages.getPages("en_US");):
    This line asynchronously fetches pages from the OneEntry API using the Pages object initialized earlier.
    .getPages("en_US") is a method call that likely retrieves a list of pages or content from the API for the specified language.

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { defineOneEntry } from 'oneentry';
 const Home = async() => {
   const { Pages } = defineOneEntry(process.env.API_URL!, {token:process.env.API_TOKEN, langCode:"en_US"});
 
-  
+  const pages = await Pages.getPages("en_US");
  
   
 }
