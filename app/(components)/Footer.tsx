3219a7e7ecf38d6fa1114c87e98a201112b6fd49
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 19:10:28 +0530

    API Call (const value = await Menus.getMenusByMarker('footer');):
    Inside the Footer component, an asynchronous API call is made to fetch menus specifically marked for the footer section.
    Menus.getMenusByMarker('footer') is likely a method provided by the Menus object returned from defineOneEntry. It retrieves menus or pages associated with the 'footer' marker.

--- app/(components)/Footer.tsx
+++ app/(components)/Footer.tsx
@@ -6,7 +6,9 @@ import React from 'react'
 const { Menus } = defineOneEntry(process.env.API_URL!, {token:process.env.API_TOKEN, langCode:"en_US"});
 
 const Footer = async () => {
-    
+    const value = await Menus.getMenusByMarker('footer');
+
+    const menus:any = value.pages;
  
     
 
