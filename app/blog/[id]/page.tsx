68fcdd4c1ace651e84b36a3c06896d1e9d23463d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-17 19:25:32 +0530

    Functional Component (const SingleArticle = async ({params}: any) => { ... }):
    SingleArticle is a functional component defined using an async arrow function. It expects params as a prop, which is likely passed from the Next.js router containing route parameters (params.id in this case).

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -5,5 +5,12 @@ import React from 'react'
 
 const { Products } = defineOneEntry(process.env.API_URL!, {token:process.env.API_TOKEN, langCode:"en_US"});
 
+const SingleArticle = async ({params}:any) => {
+    
+    
+ 
+    
 
+    
+}
 
